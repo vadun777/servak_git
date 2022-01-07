72b14d4cb154adb3809dada0a375134566b7a901
Vlad Malanin <malanin.vladislav@icloud.com>
2022-01-08 01:06:41 +0200

    Add logging user name

--- telegram-bot.py
+++ telegram-bot.py
@@ -11,6 +11,7 @@ def start_bot(update: Updater, context: CallbackContext):
 	mytext = """Привет, {}.
 
 	Как я тебя давно искал =)""".format(update.message.chat.first_name)
+	logging.info('user {} press /start'.format(update.message.chat.username))
 	update.message.reply_text(mytext)
 
 def chat(update: Updater, context: CallbackContext):
